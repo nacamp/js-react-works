1a0de27af712289b9290d9c54c552a76f0e8a435
jimmy-at-147941222526 <8nYny11JGC9a3TPeazgByab14xD/vFo8bUm1+QX7INU=>
2022-06-14 09:49:19 +0900

    [refactor]TodoCreate

--- src/components/TodoTemplate.tsx
+++ src/components/TodoTemplate.tsx
@@ -108,51 +108,150 @@ interface ITodo {
 //     );
 // }
 
+// type ITodoCreate = {
+//     message: string;
+//     count: number;
+//     disabled: boolean;
+//     /** array of a type! */
+//     names: string[];
+//     /** string literals to specify exact string values, with a union type to join them together */
+//     status: "waiting" | "success";
+//     /** any object as long as you dont use its properties (NOT COMMON but useful as placeholder) */
+//     obj: object;
+//     obj2: {}; // almost the same as `object`, exactly the same as `Object`
+//     /** an object with any number of properties (PREFERRED) */
+//     obj3: {
+//       id: string;
+//       title: string;
+//     };
+//     /** array of objects! (common) */
+//     objArr: {
+//       id: string;
+//       title: string;
+//     }[];
+//     /** a dict object with any number of properties of the same type */
+//     dict1: {
+//       [key: string]: MyTypeHere;
+//     };
+//     dict2: Record<string, MyTypeHere>; // equivalent to dict1
+//     /** any function as long as you don't invoke it (not recommended) */
+//     onSomething: Function;
+//     /** function that doesn't take or return anything (VERY COMMON) */
+//     onClick: () => void;
+//     /** function with named prop (VERY COMMON) */
+//     onChange: (id: number) => void;
+//     /** function type syntax that takes an event (VERY COMMON) */
+//     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+//     /** alternative function type syntax that takes an event (VERY COMMON) */
+//     onClick(event: React.MouseEvent<HTMLButtonElement>): void;
+//     /** an optional prop (VERY COMMON!) */
+//     optional?: OptionalType;
+//   };
+
+type ITodoCreate = {
+    onChange: (todo: ITodo) => void;
+}
+
+function TodoCreate({onChange}:ITodoCreate) {
+    const [text, setText] = useState('');
+    const [label, setLabel] = React.useState('오늘');
+
+    function handleAdd(event: any) {
+        if (text.trim() === '') {
+            console.log('node data...');
+            return;
+        }
+        console.log(text);
+        const data = {
+            id: 0,
+            text,
+            done: false,
+            label,
+        }
+        onChange(data);
+        setText('');
+    }
+
+    const handleLabel= (event: any) => {
+        console.log(event.target.value);
+        setLabel(event.target.value as string);
+    };
+
+    return (
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs={2}>
+                    <Select
+                        value={label}
+                        label="분류"
+                        onChange={handleLabel}
+                    >
+                        <MenuItem value={'오늘'}>오늘</MenuItem>
+                        <MenuItem value={'주'}>주</MenuItem>
+                        <MenuItem value={'달'}>달</MenuItem>
+                    </Select>
+                </Grid>
+                <Grid item xs={9}>
+                    <TextField fullWidth label="할일" value={text} variant="standard" onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setText(event.target.value);
+                    }} required />
+                </Grid>
+                <Grid item xs={1} style={{ display: "flex", alignItems: "right" }} >
+                    <IconButton size='small' onClick={handleAdd}>
+                        <AddIcon />
+                    </IconButton>
+                </Grid>
+            </Grid>
+        </>
+    );
+}
 
 function TodoTemplate(props: ITodoTemplate) {
     const nextId = useRef<number>(2);
-    const handleChange5 = (event: any) => {
-        console.log(event.target.value );
-        setLabel(event.target.value as string);
-    };
+
     function handleChange4(event: any, check: Number) {
         console.log('checked: ', check, event.target.checked);
-        const newInputs = inputs.map<ITodo>((todo) => 
-            todo.id === 1 ? { ...todo, done:  event.target.checked } : todo
+        const newInputs = inputs.map<ITodo>((todo) =>
+            todo.id === 1 ? { ...todo, done: event.target.checked } : todo
         );
         console.log(newInputs);
         setInputs(newInputs);
         //setChecked(event.target.checked);
     }
-    function handleChange3(event:Number) {
-        setInputs(inputs.filter((value,index) => value.id !== event ));
+    function handleChange3(event: Number) {
+        setInputs(inputs.filter((value, index) => value.id !== event));
         // console.log(event.target);
         // return state.filter(todo => todo.id !== action.id);
     }
-    function handleChange2(event: any) {
-        if( todo.trim() === ''){
-            console.log('node data...');
-            return;
-        }
-        console.log(nextId.current);
-        const data = {
-            id: nextId.current,
-            text: todo,
-            done: false,
-            label: label,
-        }
-        nextId.current++;
-        setInputs([data, ...inputs]);
-        setTodo('');
-    }
+    // function handleChange2(event: any) {
+    //     if (todo.trim() === '') {
+    //         console.log('node data...');
+    //         return;
+    //     }
+    //     console.log(nextId.current);
+    //     const data = {
+    //         id: nextId.current,
+    //         text: todo,
+    //         done: false,
+    //         label: label,
+    //     }
+    //     nextId.current++;
+    //     setInputs([data, ...inputs]);
+    //     setTodo('');
+    // }
     const [inputs, setInputs] = useState<Array<ITodo>>(initialTodos);
-    const [todo, setTodo] = useState('');
+
     const [deleteTodo, setDeleteTodo] = useState<Number>(0);
     const [checked, setChecked] = React.useState(true);
-    const [label, setLabel] = React.useState('오늘');
 
     function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
         // console.log(event.target.value);
+    }
+
+    function addTodo(todo:ITodo){
+        todo.id = nextId.current;
+        nextId.current++;
+        setInputs([todo, ...inputs]);
     }
 
     return (
@@ -167,9 +266,9 @@ function TodoTemplate(props: ITodoTemplate) {
                 <TableBody>
                     {inputs.map((row) => (
                         <TableRow key={row.id}>
-                            <TableCell><FormControlLabel value={row.id} control={<Checkbox onClick={(e:any)=>handleChange4(e, row.id)} />} label={row.text} /> <Chip label={row.label}  />  </TableCell>
+                            <TableCell><FormControlLabel value={row.id} control={<Checkbox onClick={(e: any) => handleChange4(e, row.id)} />} label={row.text} /> <Chip label={row.label} />  </TableCell>
                             <TableCell align="right" >
-                                <IconButton value={row.id} size='small' onClick={()=>handleChange3(row.id)}>
+                                <IconButton value={row.id} size='small' onClick={() => handleChange3(row.id)}>
                                     <DeleteIcon />
                                 </IconButton>
                             </TableCell>
@@ -177,29 +276,7 @@ function TodoTemplate(props: ITodoTemplate) {
                     ))}
                 </TableBody>
             </Table>
-            <Grid container spacing={2}>
-                <Grid item xs={2}>
-                    <Select
-                        value={label}
-                        label="분류"
-                        onChange={handleChange5}
-                    >
-                        <MenuItem value={'오늘'}>오늘</MenuItem>
-                        <MenuItem value={'주'}>주</MenuItem>
-                        <MenuItem value={'달'}>달</MenuItem>
-                    </Select>
-                </Grid>
-                <Grid item xs={9}>
-                    <TextField fullWidth label="할일" value={todo} variant="standard" onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                        setTodo(event.target.value);
-                    }} required />
-                </Grid>
-                <Grid item xs={1} style={{ display: "flex", alignItems: "right" }} >
-                    <IconButton size='small' onClick={handleChange2}>
-                        <AddIcon />
-                    </IconButton>
-                </Grid>
-            </Grid>
+            <TodoCreate onChange={addTodo}/>
         </>
     )
 }
